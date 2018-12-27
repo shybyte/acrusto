625ef66df1aedcabe493616e4466bad602c3d0af
shybyte <ein@volloeko.de>
2018-12-27 17:38:46 +0100

    It should fit on one amiga floppy disk :-)

--- build-release.sh
+++ build-release.sh
@@ -3,3 +3,8 @@
 cargo build --release
 strip target/release/acrusto
 ls -l target/release/acr*
+
+# Disable jemalloc for smaller size
+# use std::alloc::System;
+# #[global_allocator]
+# static GLOBAL: System = System;
