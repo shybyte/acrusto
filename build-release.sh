7fbfeb2f400c9f6d1ab1432ab581c94ac44f57d5
shybyte <ein@volloeko.de>
2018-12-29 18:23:56 +0100

    Optimizing binary file size

--- build-release.sh
+++ build-release.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 # sudo apt-get install pkg-config libssl-dev
 cargo build --release
+# cargo bloat --release --crates -n 30
+ls -l target/release/acrusto
 strip target/release/acrusto
-ls -l target/release/acr*
+ls -l target/release/acrusto
+~/opt/upx-3.95/upx target/release/acrusto
+ls -l target/release/acrusto
 
 # Disable jemalloc for smaller size
 # use std::alloc::System;
